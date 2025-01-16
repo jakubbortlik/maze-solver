90d964d6449a83bc0a680243c39bb0ed73bffa0b
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-01-16 09:58:00 +0100

    feat: break walls recursively

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 import random
 import time
+from typing import Iterable
 from graphics import Cell, Window
 
 
@@ -26,6 +27,7 @@ class Maze:
             random.seed(seed)
         self.create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(random.randrange(num_rows), random.randrange(num_cols))
 
     def create_cells(self):
         self._cells = []
@@ -46,20 +48,48 @@ class Maze:
             for row in range(self.num_rows):
                 self._draw_cell(col, row)
 
-    def _draw_cell(self, i, j):
+    def _draw_cell(self, i, j, sleep=0.01):
         self._cells[i][j].draw()
         self._animate()
+        time.sleep(sleep)
 
     def _animate(self):
         if self.win is not None:
             self.win.redraw()
-            time.sleep(0.01)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0, 0)
         self._cells[-1][-1].has_bottom_wall = False
         self._draw_cell(-1, -1)
+    
+    def _get_neighbor_opts(self, i, j) -> Iterable[tuple[int, int, str, str]]:
+        """Yield possible neighbors of a cell."""
+        if i > 0:
+            yield (i-1, j, "right", "left")
+        if i < self.num_rows - 1:
+            yield (i+1, j, "left", "right")
+        if j > 0:
+            yield (i, j-1, "bottom", "top")
+        if j < self.num_cols - 1:
+            yield (i, j+1, "top", "bottom")
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            for neighbor_opts in self._get_neighbor_opts(i, j):
+                neighbor = self._cells[neighbor_opts[0]][neighbor_opts[1]]
+                if not neighbor.visited:
+                    to_visit.append(neighbor_opts)
+            if len(to_visit) == 0:
+                self._draw_cell(i, j, sleep=0.01)
+                return
+            _i, _j, neighbor_wall, my_wall = random.choice(to_visit)
+            setattr(self._cells[_i][_j], f"has_{neighbor_wall}_wall", False)
+            setattr(self._cells[i][j], f"has_{my_wall}_wall", False)
+            self._draw_cell(_i, _j, sleep=0.01)
+            self._break_walls_r(_i, _j)
 
 
 def main():
